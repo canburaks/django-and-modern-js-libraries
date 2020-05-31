5aff975103a701a7bd5e67ccf8a2f9f714649faa
Can Burak S <canburak@msn.com>
2020-06-01 00:25:38 +0300

    backend-done

---
+++ tutorial-env/lib/python3.7/site-packages/graphene_django/__init__.py
@@ -0,0 +1,11 @@
+from .fields import DjangoConnectionField, DjangoListField
+from .types import DjangoObjectType
+
+__version__ = "2.10.1"
+
+__all__ = [
+    "__version__",
+    "DjangoObjectType",
+    "DjangoListField",
+    "DjangoConnectionField",
+]
